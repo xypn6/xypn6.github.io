105792c2274bacd257a7caedb98453a96888cf1b
Sam Pearson <56473050+xypn6@users.noreply.github.com>
2022-09-19 15:29:36 +0100

    v 0.1
    changelog:
    - added function "login_check" to get user details to send to backend

---
+++ script.js
@@ -0,0 +1,5 @@
+function login_check() {
+    var usr = document.getElementById("username_input_box").value;
+    var psw = document.getElementById("password_input_box").value;
+    alert("username: " + usr + " | password: " + psw);
+}
